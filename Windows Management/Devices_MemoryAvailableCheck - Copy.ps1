04a2e4e324ed54f11c5c4082c1322e2a4fc3e0d3
Randall Oshita <54516717+randalloshita@users.noreply.github.com>
2023-07-08 11:15:29 -0700

    initial upload

---
+++ Windows Management/Devices_MemoryAvailableCheck - Copy.ps1
@@ -0,0 +1,54 @@
+﻿$Devices = Get-Content "C:\Dev\Scripts\Devices.txt"
+
+ 
+#$Report = @()
+ 
+#Looping each server
+Foreach($Device in $Devices)
+{
+    #Write-Host "Processing $Device" -ForegroundColor Green
+    
+    $DeviceInfo = Get-CIMInstance Win32_OperatingSystem 
+
+    $DeviceName = $DeviceInfo.CSName
+    $DeviceTotalVisibleMemorySize = [math]::Round((($DeviceInfo.TotalVisibleMemorySize*1024)/1GB),2)
+    $DeviceFreePhysicalMemory = [math]::Round((($DeviceInfo.FreePhysicalMemory*1024)/1GB),2)
+    $DeviceFree = $DeviceFreePhysicalMemory/$DeviceTotalVisibleMemorySize
+    $DevicePCTFree = $DeviceFree.tostring("P")
+    
+    If ($DevicePCTFree -ge 45) 
+    {
+        $MemoryStatus = "OK"
+    }
+            ElseIf ($DevicePCTFree -ge 15) 
+            {
+            $MemoryStatus = "Warning"
+            }
+            Else 
+            {
+            $MemoryStatus = "Critical"
+            } 
+
+    
+
+    # Creating custom object
+  <#  $Object = New-Object PSCustomObject
+
+    $Object | Add-Member -MemberType NoteProperty -Name "Device name" -Value $Device
+    $Object | Add-Member -MemberType NoteProperty -Name "Total Memory" -Value $DeviceTotalVisibleMemorySize
+    $Object | Add-Member -MemberType NoteProperty -Name "Free Memory" -Value $DeviceFreePhysicalMemory
+    $Object | Add-Member -MemberType NoteProperty -Name "Memory % Free" -Value $DevicePCTFree
+    $Object | Add-Member -MemberType NoteProperty -Name "Memory Status" -Value $MemoryStatus
+    $Object  
+    $Report += $Object
+
+  #>
+  write-Host "Device name" $DeviceName
+  write-Host "Total Memory" $DeviceTotalVisibleMemorySize
+  write-Host "Free Memory" $DeviceFreePhysicalMemory
+  write-Host "Memory % Free" $DevicePCTFree
+  Write-Host "Memory Status" $MemoryStatus      
+} 
+ 
+#Display results
+#return $Report
---
+++ Windows Management/Get memory free.ps1
@@ -0,0 +1,28 @@
+﻿###   Author: Randall Oshita
+###   Date: 04112023
+###   Desc: Status report of Free Memory
+###
+###   NOTES
+###   FreePhysicalMemory
+###   Number, in kilobytes, of physical memory currently unused and available.
+###   Results are to be expressed in GB from KB, convert to bytes needed.ie., ($_*1024/GB)OR ($_ /MB)
+###
+###   TotalVisibleMemorySize
+###   Total amount, in kilobytes, of physical memory available to the operating system. This value does not necessarily indicate the true amount of physical memory, but what is reported to the operating system as available to it.
+
+$PCInfo = Get-CIMInstance Win32_OperatingSystem 
+
+
+$PCTotalVisibleMemorySize = $PCInfo.TotalVisibleMemorySize
+#$Temp_TVM = ($PCInfo.TotalVisibleMemorySize*1024)/1GB
+$PCTotalVisibleMemorySize = [math]::Round((($PCInfo.TotalVisibleMemorySize*1024)/1GB),2)
+$PCTotalVisibleMemorySize
+
+#$Temp_FPM = ($PCInfo.FreePhysicalMemory*1024)/1GB
+#$PCFreePhysicalMemory = [math]::Round($Temp_FPM,2)
+$PCFreePhysicalMemory = [math]::Round((($PCInfo.FreePhysicalMemory*1024)/1GB),2)
+$PCFreePhysicalMemory
+
+$PCPCTFree = $PCFreePhysicalMemory/$PCTotalVisibleMemorySize
+$PCPCTFree.tostring("P")
+
